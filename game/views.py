6502944cbeda3f11000b053afa7afcb002319f14
attackoncs <2710651793@qq.com>
2023-05-07 22:24:09 +0800

    write first page

--- game/views.py
+++ game/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    h1 = "<h1 style='text-align:center'>Hello, world</h1>"
+    img = "<img src='https://www.mvprpg.com/upload/article2019-08/20190826102429_365.jpg' style='width:100%'>"
+    h1 += img
+
+    return HttpResponse(h1)
+
+
+def play(request):
+    play = '<h1 style="text-align:center">游戏界面</h1>'
+    return HttpResponse(play)
