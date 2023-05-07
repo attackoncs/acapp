513dd4406b56ca4e2b3524445198181ecf487c21
attackoncs <2710651793@qq.com>
2023-05-07 23:20:57 +0800

    create project structure

--- acapp/urls.py
+++ acapp/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.urls import include
 
 urlpatterns = [
-    path('game/', include('game.urls')),
+    # path('game/', include('game.urls')),
     path('admin/', admin.site.urls),
 ]
